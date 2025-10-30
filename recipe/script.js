b6082a7f2cab0c29157cb1fa0c480c1c246d3440
Adrian Guzman <adrian1217guzman@gmail.com>
2025-10-30 00:11:48 -0700

    adds ingredientList to ul parent of ingredient list items

--- recipe/script.js
+++ recipe/script.js
@@ -40,4 +40,5 @@ for(let i = 0; i < groceryList.length; i++) {
     let listItem = groceryList[i]
     let checkbox = document.createElement('input')
     checkbox.type = 'checkbox'
+    listItem.appendChild(checkbox)
 }
