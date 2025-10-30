6364161146edc6c16440984df6657f9c5ed33ea0
Adrian Guzman <adrian1217guzman@gmail.com>
2025-10-29 20:18:03 -0700

    add myDiv variable and assign to myDiv id

--- recipe/script.js
+++ recipe/script.js
@@ -5,3 +5,6 @@ let newListItem = document.createElement('li')
 newListItem.textContent = '1 tablespoon of warm canned clam juice'
 
 document.querySelector('ul').appendChild(newListItem)
+
+const myDiv = document.getElementById("myDiv")
+
