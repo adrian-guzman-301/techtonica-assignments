66460df559597179a81055506ca657af15945bbf
Adrian Guzman <adrian1217guzman@gmail.com>
2025-11-27 01:35:26 -0800

    add pseudo-code

--- dragonslayer/script.js
+++ dragonslayer/script.js
@@ -6,3 +6,33 @@ let hitCounter = 0;
 let missCounter = 0;
 let dragonHealth = 100;
 
+//swingCounter variable needs to be rendered in the swing div
+//hitsCounter variable needs to be rendered in the hit div
+//missCounter variable needs to be rendered in the miss div
+//dragonHealth variable needs to be rendered in the dragon health div
+
+//make an array of swing success strings that will appear if hit successful
+  //if hit successful, retrieve a string at random and display it on the webpage
+
+//make an array of swing miss strings that will appear of hit fails to connect
+  //if hit misses, retrieve a string at random and display it on the webpage
+
+//make an array of strings of the different ways a dragon can kill you
+  //if dragon kills knight, retrieve a string at random and display it on the webpage
+
+//make an array of strings of the different ways you can kill a dragon
+  //if knight kills dragon, retrieve a string at random and display it on the webpage
+
+//an event listener needs to be placed on the swing button
+
+//once the user clicks on the swing button, a function needs to run that calculates a 50% chance if the swing is successful or not
+
+//inside the function, swingCounter goes up by one and increases swing counter by 1
+
+//if the swing hits, hit counter goes up by one and dragon health goes down by 25%
+
+//if swings reach 10 and dragon health is not at 0, dragon kills player
+  //target player's death array and randomly retrieve a string to display in respective div
+
+//if hits reach 4 and swings are 10 or less, player kills dragon
+  //target dragon's death array and randomly retrieve a string to display in respective div
