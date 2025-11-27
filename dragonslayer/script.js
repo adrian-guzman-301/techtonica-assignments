62c9bbd45f6dd8e1799d4c07a78bec38ce5c6bd3
Adrian Guzman <adrian1217guzman@gmail.com>
2025-11-27 12:15:18 -0800

    add pseudo-code

--- dragonslayer/script.js
+++ dragonslayer/script.js
@@ -1,10 +1,10 @@
 let song = document.getElementById('song');
 song.volume = 0.01
 
-let swingCounter = 0;
-let hitCounter = 0;
-let missCounter = 0;
-let dragonHealth = 100;
+//make dragon array with nested arrays of descriptors
+
+
+//make knight/player array with nested arrays of descriptors
 
 //swingCounter variable needs to be rendered in the swing div
 //hitsCounter variable needs to be rendered in the hit div
