8704f6e79943eaec26f2b063a2789d07001889f2
Adrian Guzman <adrian1217guzman@gmail.com>
2025-12-12 00:17:45 -0800

    add image child prop

--- puppy-or-poopy/src/App.jsx
+++ puppy-or-poopy/src/App.jsx
@@ -6,11 +6,12 @@ import './App.css'
 function App() {
   const [coinsFlipped, setCoinsFlipped] = useState(0);
   const [dogsAdopted, setDogsAdopted] = useState(0);
-  const [poopCollected, setPoopCollected] = useState(0);
+  const [poopsCollected, setPoopsCollected] = useState(0);
   const [currentImage, setCurrentImage] = useState(null)
   return (
     <>
       <h1>puppy or poopy?</h1>
+      {currentImage ? <img src = {currentImage} /> : <span>click the button to see some dogs! or some poop</span>}
       <button>flip coin</button>
     </>
   )
