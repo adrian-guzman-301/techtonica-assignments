6d94049d2eb2b0bf965685b9099a4b2fb2b0f359
Adrian Guzman <adrian1217guzman@gmail.com>
2025-12-19 14:16:47 -0800

    add coin flip component

--- puppy-or-poopy/src/App.jsx
+++ puppy-or-poopy/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -11,8 +9,7 @@ function App() {
   return (
     <>
       <h1>puppy or poopy?</h1>
-      {currentImage ? <img src = {currentImage} /> : <span>click the button to see some dogs! or some poop</span>}
-      <button>flip coin</button>
+      <CoinFlipControls />
     </>
   )
 }
