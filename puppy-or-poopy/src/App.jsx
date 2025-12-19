47713185c6a3c23f1185a92b58da68b875e1af04
Adrian Guzman <adrian1217guzman@gmail.com>
2025-12-19 14:29:50 -0800

    remove states

--- puppy-or-poopy/src/App.jsx
+++ puppy-or-poopy/src/App.jsx
@@ -2,10 +2,6 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [coinsFlipped, setCoinsFlipped] = useState(0);
-  const [dogsAdopted, setDogsAdopted] = useState(0);
-  const [poopsCollected, setPoopsCollected] = useState(0);
-  const [currentImage, setCurrentImage] = useState(null)
   return (
     <>
       <h1>puppy or poopy?</h1>
