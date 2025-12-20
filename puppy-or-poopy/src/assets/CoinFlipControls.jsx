21f0badfb9b4dfef59700e3aa000846523e9c39d
Adrian Guzman <adrian1217guzman@gmail.com>
2025-12-19 16:24:24 -0800

    add parameters

--- puppy-or-poopy/src/assets/CoinFlipControls.jsx
+++ puppy-or-poopy/src/assets/CoinFlipControls.jsx
@@ -1,7 +1,8 @@
-export default function CoinFlipControls() {
+export default function CoinFlipControls({ onFlip, flipCount }) {
   return (
-    <>
+    <div>
       <button onClick={onFlip}>flip coin</button>
-    </>
+      <p>flips: {flipCount}</p>
+    </div>
   )
 }
