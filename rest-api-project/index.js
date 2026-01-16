951c5cac50c975f7115b0834b5ab4698b2342a2c
Adrian Guzman <adrian1217guzman@gmail.com>
2026-01-15 17:48:18 -0800

    edit put codeblock

--- rest-api-project/index.js
+++ rest-api-project/index.js
@@ -19,8 +19,8 @@ app.post('/cards', (req, res) => {
 //PUT: replace entire card collection with new cards
 app.put('/cards', (req, res) => {
   //find card
-  findCard = CARDS.find((card) => {
-    return card.id !== req.params.id
+  const findCard = CARDS.find((card) => {
+    return card.id === req.params.id
   })
   //update found card's properties
   //send response
