5b94fa742c3141ce7d4691456c96cf430a931951
Adrian Guzman <adrian1217guzman@gmail.com>
2026-01-22 13:10:20 -0800

    define GET route to root URL

--- weather-app/server.js
+++ weather-app/server.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+
+//define a route for GET requests to the root URL
+app.get('/', (req, res) => {
+  res.send('hello techtonica from express!')
+});
