0a7c30f547be9e91ae0488545a1905007d112ae0
Adrian Guzman <adrian1217guzman@gmail.com>
2026-01-26 15:10:58 -0800

    test hardcoded weather data

--- weather-app/vite-weather-app/src/App.jsx
+++ weather-app/vite-weather-app/src/App.jsx
@@ -3,10 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SubmitButton from './submit-button.jsx'
-import WeatherDisplay from '.WeatherDisplay'
+import WeatherDisplay from '.WeatherDisplay.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  //test data
+  const test = {
+    main: { temp: 5000, humidity: 999999 },
+    wind: {speed: 777 },
+    weather: [{ description: "hellfire" }]
+  }
 
   return (
     <>
