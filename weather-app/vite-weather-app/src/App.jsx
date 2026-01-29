0cd5537faa3fb5b0a8d7dae90c21d903333b070d
Adrian Guzman <adrian1217guzman@gmail.com>
2026-01-28 16:00:18 -0800

    remove starter code

--- weather-app/vite-weather-app/src/App.jsx
+++ weather-app/vite-weather-app/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import SubmitButton from './SubmitButton.jsx'
 import WeatherDisplay from './WeatherDisplay.jsx'
