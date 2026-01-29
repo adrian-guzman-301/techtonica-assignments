3f10d4216b58a3f08276b5a1fb2872a4fc79831c
Adrian Guzman <adrian1217guzman@gmail.com>
2026-01-29 12:12:45 -0800

    add fahrenheit scale

--- weather-app/vite-weather-app/src/WeatherDisplay.jsx
+++ weather-app/vite-weather-app/src/WeatherDisplay.jsx
@@ -2,7 +2,7 @@ function WeatherDisplay({ weatherData }) {
   return (
     <div>
       <h2>Weather Info</h2>
-      <p>Temp: {weatherData.main.temp}</p>
+      <p>Temp: {weatherData.main.temp}°F</p>
       <p>Wind speed: {weatherData.wind.speed} mph</p>
       <p>Vibe: {weatherData.weather[0].description}</p>
       <p>Humidity: {weatherData.main.humidity}%</p>
