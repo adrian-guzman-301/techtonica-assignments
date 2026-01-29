2354ea673b08b13a36ff2a7d6064164137f89b0c
Adrian Guzman <adrian1217guzman@gmail.com>
2026-01-29 09:58:23 -0800

    add API get request

--- weather-app/server.js
+++ weather-app/server.js
@@ -4,9 +4,9 @@ const PORT = 8080;
 require('dotenv').config();
 
 //define a route for GET requests to the root URL
-app.get('/', (req, res) => {
-  res.send('hello techtonica from express!')
-});
+// app.get('/', (req, res) => {
+//   res.send('hello techtonica from express!')
+// });
 
 //start server
 app.listen(PORT, () => {
@@ -14,21 +14,41 @@ app.listen(PORT, () => {
 })
 
 //test hardcoded fetch request
-async function testWeatherFetch() {
-  const city = 'Anaheim'
+// async function testWeatherFetch() {
+//   const city = 'Anaheim'
+//   const apiKey = process.env.API_KEY;
+//   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+//   try {
+//     const response = await fetch(url);
+//     if(!response.ok) {
+//       throw new Error(`Response status: ${response.status}`)
+//     }
+
+//     const result = await response.json();
+//     console.log(result);
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
+
+// testWeatherFetch()
+
+app.get("/weather", (req, res) => {
+  const city = req.query.cityName;
   const apiKey = process.env.API_KEY;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-  try {
-    const response = await fetch(url);
-    if(!response.ok) {
-      throw new Error(`Response status: ${response.status}`)
-    }
-
-    const result = await response.json();
-    console.log(result);
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-testWeatherFetch()
+  const params = new URLSearchParams({
+    q: req.query.cityName,
+    appid: process.env.API_KEY,
+    units: "imperial",
+  });
+  const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      res.send({ data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
