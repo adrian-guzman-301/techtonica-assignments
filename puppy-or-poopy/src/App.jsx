23d16a59d720fc9db77b8f26a6589c4f0a1979eb
Adrian Guzman <adrian1217guzman@gmail.com>
2026-01-30 15:28:32 -0800

    committing whole project

--- puppy-or-poopy/src/App.jsx
+++ puppy-or-poopy/src/App.jsx
@@ -1,13 +1,16 @@
 //hook that manages data
 import {useState} from 'react'
 import './App.css'
-import CoinFlipControls from './CoinFlipControls'
-import DogImageRender from './DogImageRender'
+import CoinFlipControls from './assets/CoinFlipControls'
+import DogImageRender from './assets/DogImageRender'
 
 
 function App() {
+  //store number from 0 and update
   const [flipCount, setFlipCount] = useState(0)
+  //store url and show current image
   const [currentImage, setCurrentImage] = useState('')
+  //heads or tails result and update what resulted
   const [flipResult, setFlipResult] = useState('')
   //coin flip funciton
   const coinFlip = () => {
@@ -15,6 +18,7 @@ function App() {
     //state update for coin flip
     setFlipCount(flipCount + 1)
     setFlipResult(result)
+    setCurrentImage(result) //heads will fetch a dog picture from dog ceo website
   }
   return (
     <>
