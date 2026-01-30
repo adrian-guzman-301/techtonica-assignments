42733e87543eb4610f6adcd6da52f7a9d9630bc3
Adrian Guzman <adrian1217guzman@gmail.com>
2026-01-30 10:45:44 -0800

    display weather icon

--- weather-app/vite-weather-app/src/WeatherDisplay.jsx
+++ weather-app/vite-weather-app/src/WeatherDisplay.jsx
@@ -5,6 +5,7 @@ function WeatherDisplay({ weatherData }) {
   return (
     <div>
       <h2>Weather Info</h2>
+      <img src={iconUrl} alt="weather icon" />
       <p>Temp: {weatherData.main.temp}°F</p>
       <p>Wind speed: {weatherData.wind.speed} mph</p>
       <p>Vibe: {weatherData.weather[0].description}</p>
