0a02a0256511ece5848e3103ea172c3fb4f5d49d
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-05 11:40:23 -0800

    add pseudocode

--- trivia-game/server/server.js
+++ trivia-game/server/server.js
@@ -4,8 +4,19 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  const url = `https://opentdb.com/api.php?amount=10`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      res.send({ data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 })
+//i want to edit the url to have template literals
+//the template literals should correspond to frontend input
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
