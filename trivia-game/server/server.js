a1bba41f399629fb4aa6214bd210bb5742185ac4
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-04 18:19:16 -0800

    complete test get route

--- trivia-game/server/server.js
+++ trivia-game/server/server.js
@@ -1,1 +1,12 @@
 //get route
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
