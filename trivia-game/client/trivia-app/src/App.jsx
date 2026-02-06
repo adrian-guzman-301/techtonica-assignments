e522be8df0c6d0fd80cabd711788c37556cca40f
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-06 13:03:04 -0800

    add components

--- trivia-game/client/trivia-app/src/App.jsx
+++ trivia-game/client/trivia-app/src/App.jsx
@@ -2,34 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import GameResult from './GameResult'
+import GamePlay from './GamePlay'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  //const [count, setCount] = useState(0)
+  const [isWinner, setIsWinner] = useState(null)
+  const [questions, setQuestions] = useState([])
+  //setIsWinner(true)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <div>this is the game setup</div>
+      <GamePlay resultHandler={setIsWinner}/>
+      <GameResult winOrLoss={isWinner}/>
+    </div>
+    )
 }
 
 export default App
