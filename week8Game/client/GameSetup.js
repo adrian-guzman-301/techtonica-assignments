faa2e37bcfe454e2b29fd727461c6559be435b38
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-11 10:44:04 -0800

    add defaults

--- week8Game/client/GameSetup.js
+++ week8Game/client/GameSetup.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 const GameSetup = (props) => {
   //10 questions is the default
   const [questionCount, setQuestionCount] = useState(10)
-  const [category, setCategory] = useState()
-  const [difficulty, setDifficulty] = useState()
-  const [questionType, setQuestionType] = useState()
+  const [category, setCategory] = useState(9)
+  const [difficulty, setDifficulty] = useState('easy')
+  const [questionType, setQuestionType] = useState('boolean')
 }
