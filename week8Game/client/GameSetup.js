58e768edcd5a8c2b990233f8b2377e3ea5cd14b5
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-11 12:09:02 -0800

    add questionCount options

--- week8Game/client/GameSetup.js
+++ week8Game/client/GameSetup.js
@@ -10,7 +10,12 @@ const GameSetup = (props) => {
   return (
     <>
       <h2>pick your question preferences! or you can just hit start to jump right in</h2>
-      {}
+      <select value={questionCount} onChange={(event) => setQuestionCount(event.target.value)}>
+        <option value={5}>5 questions</option>
+        <option value={10}>10 questions</option>
+        <option value={15}>15 questions</option>
+      </select>
+      
     </>
   )
 }
