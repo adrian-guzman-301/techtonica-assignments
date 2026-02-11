d1644d349825a330f92f5e3d3d1579c018a7e6d2
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-11 13:33:07 -0800

    track if game has started

--- week8Game/client/src/App.js
+++ week8Game/client/src/App.js
@@ -3,20 +3,22 @@ import UserForm from './components/user.js';
 import Game from './components/game.js';
 import './App.css';
 import { useState } from "react";
-import GameSetup from './components/GameSetup.js'
+import GameSetup from './components/GameSetup'
 
 function App() {
   const [user, setUser] = useState("");
+  const [gameStarted, setGameStarted] = useState(false);
   const handleUser = (text) =>{
     setUser(text);
+
   }
 
   return (
     <div className="App">
-    <Header user={user} />
-    <UserForm grabUser={handleUser} />
-    {user ? <Game /> : null}
-      
+      <Header user={user} />
+      <UserForm grabUser={handleUser} />
+      {user ? <GameSetup /> : null}
+      {user ? <Game /> : null}
     </div>
   );
 }
