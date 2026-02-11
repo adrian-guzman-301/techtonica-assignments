0bb51d49b34976b80e316a1c77bddd6c8ffa4aa4
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-11 14:20:18 -0800

    pass props

---
+++ week8Game/client/src/components/GameSetup.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const GameSetup = (props) => {
+
+  return (
+    <>
+      <h2>pick your question preferences! or you can just hit start to jump right in</h2>
+      <button onClick={props.onStartGame}>start!</button>
+      <select value={props.questionCount} onChange={(event) => props.setQuestionCount(event.target.value)}>
+        <option value={5}>5 questions</option>
+        <option value={10}>10 questions</option>
+        <option value={15}>15 questions</option>
+        <option value={20}>20 questions</option>
+      </select>
+      <select value={props.category} onChange={(event) => props.setCategory(event.target.value)}>
+        <option value={9}>general knowledge</option>
+        <option value={23}>history</option>
+        <option value={24}>politics</option>
+        <option value={15}>video games</option>
+      </select>
+
+    </>
+  )
+}
+
+export default GameSetup
