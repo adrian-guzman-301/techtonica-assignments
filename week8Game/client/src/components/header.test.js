df37b5bfd4b6558fb1ea617688cb165e0a554d3e
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-10 22:15:35 -0800

    add test pseudocode

---
+++ week8Game/client/src/components/header.test.js
@@ -0,0 +1,12 @@
+import { Header } from './header.js'
+
+/*
+thoughts about writing a test:
+  - i want to test if the header renders the user's name properly
+  step 1. make test file
+  step 2. import test subject
+  step 3. use "test" method
+    i. 'inputted username should appear onscreen'
+  step 4. use "expect" and toBe methods
+    i. expect inputted username to be 'username'
+*/
