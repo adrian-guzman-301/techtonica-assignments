699bc2ebbd5c2ff0386890e6be52a22dbe43800f
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-11 10:41:45 -0800

    add variable

--- week8Game/client/GameSetup.js
+++ week8Game/client/GameSetup.js
@@ -3,4 +3,6 @@ import { useState } from 'react';
 const GameSetup = (props) => {
   //10 questions is the default
   const [questionCount, setQuestionCount] = useState(10)
+  const [category, setCategory] = useState()
+
 }
