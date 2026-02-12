b1febf1242c4f51dc93983cc75491f54953aa559
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-12 10:36:41 -0800

    render multiple choice responses

--- week8Game/client/src/components/questioncard.js
+++ week8Game/client/src/components/questioncard.js
@@ -3,7 +3,7 @@ const QuestionCard = (props) => {
     //multiple choice
     let allAnswers = []
     if(props.question.type === 'multiple') {
-      allanswers = [props.question.correct_answer, ...props.question.incorrect_answers]
+      allAnswers = [props.question.correct_answer, ...props.question.incorrect_answers]
     }
     
 
@@ -11,11 +11,19 @@ const QuestionCard = (props) => {
       <div className={"question-section"}>
         <div className='question-text'>{props.question.question}</div>
         <div className='answer-section'>
-		<button>True</button>
-		<button>False</button>
+          {props.question.type === 'boolean' ? (
+            <>
+              <button>True</button>
+              <button>False</button>
+            </>
+          ) : (
+              allAnswers.map((answer, index) => {
+                return <button key={index}>{answer}</button>
+              })
+          )}
         </div>
       </div>
     );
-  };
+};
 
 export default QuestionCard;
