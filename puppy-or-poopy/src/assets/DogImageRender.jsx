a62f1d29d491bee14070ad97d97d9bce71527064
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-11 20:51:51 -0800

    add pseudocode

--- puppy-or-poopy/src/assets/DogImageRender.jsx
+++ puppy-or-poopy/src/assets/DogImageRender.jsx
@@ -1,5 +1,18 @@
+import { useState, useEffect } from 'react';
+
 export default function DogImageRender() {
-  return <div>
-    dog image
-  </div>
+  const [dogImage, setDogImage] = useState('');
+  
+  useEffect(() => {
+    fetch(/** */)
+      .then(/** */)
+      .then(/** */)
+  }, [])
+
+  return (
+    <div>
+      <img src={dogImage} alt="random dog" />
+    </div>
+  )
 }
+//import a random dog image from dog ceo website in the div
