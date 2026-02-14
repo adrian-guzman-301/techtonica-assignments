82548c7a42f19b523f5f52ddac14bad82c477c66
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-13 16:46:15 -0800

    edit test import

--- week8Game/client/src/components/header.test.js
+++ week8Game/client/src/components/header.test.js
@@ -1,12 +1,19 @@
-import { Header } from './header.js'
+import Header from './header.js'
+import { render, screen } from '@testing-library/react';
 
-/*
-thoughts about writing a test:
-  - i want to test if the header renders the user's name properly
-  step 1. make test file
-  step 2. import test subject
-  step 3. use "test" method
-    i. 'inputted username should appear onscreen'
-  step 4. use "expect" and toBe methods
-    i. expect inputted username to be 'username'
-*/
+
+//thoughts about writing a test:
+  //- i want to test if the header renders the user's name properly
+//step 4. use "expect" and toBe methods
+  //i. expect inputted username to be 'username'
+describe('Render Component', () => {
+  it('renders the users name', () => {
+//step 1: set up the name
+const name = 'red'
+//step 2: render a header with the set up name
+render(<Header user={name}/>)
+//step 3: verify header renders username properly
+const header = screen.getByRole('heading')
+expect(header).toHaveTextContent(`Welcome to my game, ${name}`)
+  });
+});
