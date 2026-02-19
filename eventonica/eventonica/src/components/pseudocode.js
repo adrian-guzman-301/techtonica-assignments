afc02d566d1f8d26e470a40df21327f8fa4fe3c8
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-18 16:49:49 -0800

    edit pseudocode

--- eventonica/eventonica/src/components/pseudocode.js
+++ eventonica/eventonica/src/components/pseudocode.js
@@ -1,17 +1,9 @@
 /**********************************************
 looking at the example eventonica gif, it looks like i need:
-  1. columns:
-    aa. favorite/heart option
-    a. name
-    b. date
-    c. description
-    d. category
-    e. edit
-    f. delete
-  2. components:
-    a. the entire app
-    b. row component 
-    c. event component:
+   a. components:
+    1. the entire app
+    2. row component 
+    3. event component:
       1. favorite/heart option
       2. name
       3. date
@@ -19,6 +11,7 @@ looking at the example eventonica gif, it looks like i need:
       5. category
       6. edit
       7. delete
-    d. search component
-    e. filter component
+    4. search component
+    5. filter component
+    
 **********************************************/
