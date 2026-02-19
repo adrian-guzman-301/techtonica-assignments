247947572976ef758b0296bbc74aabd4ee7c1fc2
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-18 18:39:04 -0800

    add psuedocode and create files

--- eventonica/eventonica/src/components/pseudocode.js
+++ eventonica/eventonica/src/components/pseudocode.js
@@ -3,7 +3,6 @@ looking at the example eventonica gif, it looks like i need:
    a. components:
     1. the entire app
     2. row component 
-    3. event component:
       1. favorite/heart option
       2. name
       3. date
@@ -11,7 +10,25 @@ looking at the example eventonica gif, it looks like i need:
       5. category
       6. edit
       7. delete
-    4. search component
-    5. filter component
-    
+    3. search component
+    4. filter component
+    5. add event component/edit event component--event.jsx
+
 **********************************************/
+
+
+
+
+/********************************************** 
+database schema--basic data structure
+  define which tables this database should have
+  1. use of 1 table for events and columns:
+    a. varchar Name
+    b. DATE
+    c. varchar Description
+    d. varchar Category
+    e. favorite boolean
+    f. primary key--uniquely define every table entry
+
+
+***********************************************/
