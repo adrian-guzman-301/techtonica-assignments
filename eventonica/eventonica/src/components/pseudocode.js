09b4809c50ff1931fc8338f1148d676caa4d0199
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-18 16:44:13 -0800

    first commit

---
+++ eventonica/eventonica/src/components/pseudocode.js
@@ -0,0 +1,22 @@
+/**********************************************
+looking at the example eventonica gif, it looks like i need:
+  1. columns:
+    aa. favorite/heart option
+    a. name
+    b. date
+    c. description
+    d. category
+    e. edit
+    f. delete
+  2. components:
+    a. the entire app
+    b. row component and its parts:
+      1. favorite/heart option
+      2. name
+      3. date
+      4. description
+      5. category
+      6. edit
+      7. delete
+    c. 
+**********************************************/
