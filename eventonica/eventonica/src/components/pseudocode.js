8a3ecfd2f82a63c9c43cc2f54bf01f808f693de0
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-19 19:58:45 -0800

    add pseudocode

--- eventonica/eventonica/src/components/pseudocode.js
+++ eventonica/eventonica/src/components/pseudocode.js
@@ -2,7 +2,7 @@
 looking at the example eventonica gif, it looks like i need:
    a. components:
     1. the entire app
-    2. row component 
+    2. event-info component 
       1. favorite/heart option
       2. name
       3. date
@@ -14,7 +14,27 @@ looking at the example eventonica gif, it looks like i need:
     4. filter component
     5. add event component/edit event component--event.jsx
 
+user flow:
+1. i want there to initially be some events so users can start using CRUD immediately (besides create)
+2. app.jsx would hold everything
+3. initially there is no database--i will hardcode events for now and next week will implement a database
+4. the event-info component will be a major component
+  a. it will retrieve hardcoded data
+  b. it will have all event info and buttons
+  c. buttons components will be within event-info component
+  d. 
+
+component tree:
+- App: holds all state (and hardcoded data for now)
+  - SearchBar
+  - FilterBar (favorites toggle)
+  - EventList: receives filtered events
+    - EventCard (per event): fav button, edit/delete buttons
+  - EventForm: panel for add/edit
+
+
 **********************************************/
+
 
 
 
