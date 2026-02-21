24f5d1152152ba5b3c40d26442f6a40b0f319cb2
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-20 16:33:40 -0800

    add events useState

--- eventonica/eventonica/src/App.jsx
+++ eventonica/eventonica/src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import EventCard from './EventCard'
+import EventCard from './components/EventCard'
 
-const events = [
-  { id: 1, name: 'cat rave', date: '12-31-26' , description: 'cat costumes and loud music', category: 'music' , isFavorite: true },
-  { id: 2, name: 'javascript night', date: '05/05/26' , description: 'tackle JS coding challenges alongside the techtonica community', category: 'coding' , isFavorite: true },
-  { id: 3, name: 'graduation', date: '06-25-26', description: `look back at how far you've come and look forward to exciting opportunities`, category: 'celebration' , isFavorite: false },
-]
+const [events, setEvents] = useState([])
 
 function App() {
 
