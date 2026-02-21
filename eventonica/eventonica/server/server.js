7ad5997a0e07ff7bff5419fc1b87913d857e61ae
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-21 12:16:53 -0800

    add db pseudocode

--- eventonica/eventonica/server/server.js
+++ eventonica/eventonica/server/server.js
@@ -1,15 +1,23 @@
 import express from 'express'
 import cors from 'cors'
 
-app.use(cors())
 const app = express()
 const port = 3000
+app.use(cors())
 
 const events = [
   { id: 1, name: 'cat rave', date: '12-31-26' , description: 'cat costumes and loud music', category: 'music' , isFavorite: true },
   { id: 2, name: 'javascript night', date: '05/05/26' , description: 'tackle JS coding challenges alongside the techtonica community', category: 'coding' , isFavorite: true },
   { id: 3, name: 'graduation', date: '06-25-26', description: `look back at how far you've come and look forward to exciting opportunities`, category: 'celebration' , isFavorite: false },
 ]
+
+/* 
+db schema
+ - i think i only need one table, since i am only dealing with events. the table would be able to get added to, deleted from, and edited on. 
+ 
+ - as for the columns of the table, i'd say a column for each: id, name, date, description, category, and isFavorite
+
+*/
 
 app.get('/', (req, res) => {
   res.json(events)
