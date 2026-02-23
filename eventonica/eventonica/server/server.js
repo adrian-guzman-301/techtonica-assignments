aac64c2a75c6e09c00317cca06bc906f2450cb25
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-23 12:24:57 -0800

    remove hardcoded test data

--- eventonica/eventonica/server/server.js
+++ eventonica/eventonica/server/server.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { json } from 'express'
 import cors from 'cors'
 import pg from 'pg'
 const { Pool, Client } = pg
@@ -8,13 +8,7 @@ const port = 3000
 //tell my browser it's okay
 app.use(cors())
 //enable express to use req.body
-app.use(express.json())
-
-const events = [
-  { id: 1, name: 'cat rave', date: '12-31-26' , description: 'cat costumes and loud music', category: 'music' , isFavorite: true },
-  { id: 2, name: 'javascript night', date: '05/05/26' , description: 'tackle JS coding challenges alongside the techtonica community', category: 'coding' , isFavorite: true },
-  { id: 3, name: 'graduation', date: '06-25-26', description: `look back at how far you've come and look forward to exciting opportunities`, category: 'celebration' , isFavorite: false },
-]
+app.use(json())
 
 const client = new Client({
   user: 'tpl925_3',
