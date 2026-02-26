66bc2c4edf59bef3b18a3638252e1c13a8826048
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-25 16:01:12 -0800

    add fetch

--- eventonica/eventonica/src/components/Form.jsx
+++ eventonica/eventonica/src/components/Form.jsx
@@ -11,11 +11,25 @@ function Form() {
     setEventName(event.target.eventName)
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch(`http://localhost:3000` {
+      method: 'POST',
+      body: JSON.stringify({
+        name: eventName,
+        date: date,
+        description: description,
+        category: category,
+        isFavorite: isFavorite
+      })
+    })
+  }
+
   return (
     <>
       <form action={`http://localhost:3000`} onSubmit={''}>
         <label for="name">event name:</label>
-        <input id="name"/>
+        <input id="name" value={eventName} onChange={handleNameChange}/>
         <label for="date">date:</label>
         <input id="date" type="date"/>
         <label for="description">description:</label>
