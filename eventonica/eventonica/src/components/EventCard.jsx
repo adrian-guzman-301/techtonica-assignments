e24c9eb9554c3a4f7f5671fd2bb2e33d320f3f58
Adrian Guzman <adrian1217guzman@gmail.com>
2026-02-26 18:11:13 -0800

    display event id

--- eventonica/eventonica/src/components/EventCard.jsx
+++ eventonica/eventonica/src/components/EventCard.jsx
@@ -3,6 +3,7 @@ import '../App.jsx'
 function EventCard(props) {
   return (
     <>
+      <p>id: {props.event.id}</p>
       <p>name: {props.event.name}</p>
       <p>date: {props.event.date}</p>
       <p>description: {props.event.description}</p>
