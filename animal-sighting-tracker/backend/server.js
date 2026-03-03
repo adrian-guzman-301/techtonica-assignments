6764a9ff44619bc3affa70fee39c2a185d131f99
Adrian Guzman <adrian1217guzman@gmail.com>
2026-03-03 12:48:56 -0800

    add database object

--- animal-sighting-tracker/backend/server.js
+++ animal-sighting-tracker/backend/server.js
@@ -1,3 +1,8 @@
+//no initialization options
+const pgp = require('pg-promise')
+//create database object from connection
+const db = pgp(connection)
+
 //hello world example
 const express = require('express')
 const app = express()
@@ -5,4 +10,8 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send(`hello from express!`)
+})
+
+app.listen(port, () => {
+  console.log(`this example app's ears pricked up on port ${port}`)
 })
